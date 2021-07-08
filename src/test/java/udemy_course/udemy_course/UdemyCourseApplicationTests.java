9a8a7fe3019f89653e92a3bf0bfd090a1536ad5f
ptkgupta <87114823+ptkgupta@users.noreply.github.com>
2021-07-08 12:31:51 +0530

    Initial commit
    Initial commit

---
+++ src/test/java/udemy_course/udemy_course/UdemyCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package udemy_course.udemy_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
